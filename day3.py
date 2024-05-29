7aa5052cd876def3834287b59ac0eacaa50e56ca
aashilchaudhary <chaudharyashil317@gmail.com>
2024-05-29 13:21:50 +0530

    ashil

---
+++ day3.py
@@ -0,0 +1,136 @@
+#dt={
+#   "name":"mohan",
+#    "branch":"cse",
+#    "year":2,
+#    "year":3,
+#    "class":["oae","two","three"],
+#    "year":("1","2","3"),
+#    "set":(True,"hello",1),
+#}
+#print(dt)
+#print(type(dt))
+#print(len(dt))
+#dt={
+#    "brand":"ford",
+#    "model": "GT",
+#    "year": 1919
+#}
+#print(dt.items)
+#print(dt.update({"year":2020}))
+#print(dt)
+#print(dt.pop("model"))
+#print(dt)
+#dt={'dt':{"name":"ram"},
+#   'dt2':{"name2":"mohan"}}
+#print(dt)
+#print(dt['dt2']["name2"])
+
+#a=200
+#b=33
+#if a>b: print("a is greater than b")
+
+#a=2
+#b=330
+#print("a")if a>b else print("b")    
+
+#a=330
+#b=330
+#print("A") if a>b else print("equal") if a==b else print("b")
+
+#a=200
+#b=33
+#c=500
+#if a>b and c>a:
+#   print("both conditions are true")
+
+#a=33
+#b=200
+#if not a>b:
+#     print("a is not greater than b")
+#x=41
+#if x>10:
+#    print("above ten"),
+#if x>20:
+#    print("above 20"),
+#else:
+#    print("but not above than 20")
+
+#x=41
+#if x>10:
+#    print("above ten"),
+#if x>20:
+#    pass
+#else:
+#    print("but not above than 20")
+
+#x=input("enter alphabet:")
+#y=input("enter alphabet:")
+#if x==y:
+#    print("cond is true")
+#else:
+#    print("cond is not true")
+
+#x=input("enter alphabet:")
+#if x==('a','e','i','o','u'):
+#    print("vowels")
+#else:
+#    print("not vowels")
+
+#for x in "mango":
+#    print(x)
+
+#fruits=["apple","mango","cherry"]
+#for x in fruits:
+#    print(x)
+#    if x=="mango":
+#        break
+
+#fruits=["apple","mango","cherry"]
+#for x in fruits:
+#    print(x)
+#    if x=="mango":
+#        continue
+
+#for x in range(6):
+#    print(x)
+
+#for x in range (2,10):
+#    print(x)
+
+#for x in range(6):
+#    print(x)
+#else:
+#    print("finished")
+
+#for x in range (6):
+#    if x==3: break
+#    print(x)
+#else:
+#    print("finish")
+
+#dt={
+#    "name"= "ashil",
+#    "branch"="cse",
+#    "year":2,
+#    "year":3,
+#    "class":["one","two","three"]
+#    "year":("1","2","3"),
+#    "set":{True,"hello",1}
+#}
+#for x in dt:
+#print(dt[x])
+
+#for x in dt.keys():
+#print(x)
+
+#for x in dt.values():
+#print(x)
+
+
+#y= int(input("enter number:"))
+#for i in range(1,11):
+#    print(f*(y)*(i)=(y*i)*)
+
+fruits =["apple","mango","cherry"]
+for index, fruits in enumerate(fruits):
+    
---
+++ day4.py
@@ -0,0 +1,96 @@
+#i=1
+#while i<6:
+#    print(i)
+#    i+=1
+
+#i=1
+#while i<6:
+#    print(i)
+#    if i==3:
+#        break
+#    i+=1
+
+#i=0
+#while i<6:
+#    i+=1
+#    if i==3:
+#       continue
+#    print(i)
+
+#def my_funcion():
+#    print("hello from a fucntion")   
+## calling a function
+#my_funcion()    
+
+# def my_function1(fname):
+#     print(fname+"sharma")
+# my_function1("ashil")
+# my_function1("sachin")
+
+# def add(num1,num2):
+#     result=num1+num2
+#     return result
+# num1=200
+# num2=400
+# result=add(num1,num2)
+# print(result)
+
+# def my_name(fname+lname)
+# print(fname+" "+lname)
+# my_name("ashil","chaudhary")
+
+# def my_function2(*kids):
+#     print("the youngest child is "+kids[2])
+# my_function2("ram","mohan","rohan")
+
+# def my_function3(child1 ,child2 ,child3):
+#     print("the youngest child is "+ child3)
+# my_function3(child1="ram",child2="rohan",child3="mohan")
+
+# def my_country(country="india"):
+#     print("i am from "+ country)
+# my_country("sweden")
+# my_country()
+# my_country("brazil")
+
+# def sqr(num):
+#     return num*num
+# num=int(input("enter the number"))
+# print("square of th enumber",sqr(num))
+
+# def square_element(input_1st):
+#     temp=[]
+#     for x in input_1st:
+#         temp.append(x**2)
+#     return temp
+# output=square_element([2,4,7,9])
+# print(output)
+
+# def avg_function(input_1st):
+#     return sum(input_1st)/len(input_1st)
+# print(avg_function([25,56,78,90]))
+
+# a=[1,2,3,4,5,6,7]
+# def minimum(a):
+#     min=a[0]
+#     for i in a:
+#         if i<min:
+#             min=i
+#     return min
+# print("minimum value is ", minimum(a))
+
+#lambda arg:expression
+# x=lambda a:a+10
+# print(x(10))
+
+# x=lambda a,b:a*b
+# print(x(10,20))
+
+# x=lambda a,b,c:a+b+c
+# print(x(20,30,40))
+
+# def my_function4(n):
+#     return lambda a:a*n
+# double=my_function4(2)
+# print(double(11))
+
